801bc0bbb4866f30fc6aef56807e14a3de366abe
JustNigel <nigelcamba@gmail.com>
2024-07-26 14:59:58 +0800

    code commitment

--- routes/web.php
+++ routes/web.php
@@ -2,26 +2,20 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('/admin/users')->name('admin.')->group(function () {
-    Route::get('/management', function () {
-        return '<h1 style="color: red">Hello World</h1>';
-    })->name('users.management');
 
-    Route::get('/reports', function () {
-        return '<h1 style="color: red">Hello World</h1>';
-    })->name('users.reports');
+Route::prefix('/user') -> name("user.") -> group(function() {
+    Route::get('/profile', function () {
+        return '<h1 style="color: red">Nigel Camba</h1>
+        <h2 style="color: blue">Oroqueta St.</h2>
+        <h3 style="color: green">nigelcamba@gmail.com</h3>';
+    }) ->name('profile');
+    Route::get('/work-experience', function () {
+        return '<h1 style="color: green">Web Dev</h1>';
+    }) ->name('work-experience');
+
+    Route::get('/', fn() => to_route("user.profile")) ->name('home');
 });
 
-// Route::get('/admin/users/management', function () {
-//     return '<h1 style="color: red">Hello World</h1>';
-// })->name('admin.users.management');
+Route::view('/', "welcome");
 
-// Route::get('/admin/users/reports', function () {
-//     return '<h1 style="color: red">Hello World</h1>';
-// })->name('admin.users.reports');
-
-Route::view('/', 'welcome');
-
-Route::get('/home', function () {
-    return to_route('admin.users.management');
-});
+// Route::redirect('/', '/user/profile') ->name('home');
